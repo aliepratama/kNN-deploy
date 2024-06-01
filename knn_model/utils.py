b0709e87462bd6c83b4e6868b3176fceb04c83ee
aliepratama <aliepratama2@gmail.com>
2024-06-01 21:10:14 +0700

    fix: refactor the scaler data prep

--- knn_model/utils.py
+++ knn_model/utils.py
@@ -2,20 +2,15 @@ import pickle, pandas, numpy, os
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
 
-df = pickle.load(open(f'{os.getcwd()}/knn_model/trained_models/dataframe.pkl', 'rb'))
 model = pickle.load(open(f'{os.getcwd()}/knn_model/trained_models/model_v1.0.0.pkl', 'rb'))
+scaler = pickle.load(open(f'{os.getcwd()}/knn_model/trained_models/scaler.pkl', 'rb'))
 
 def preprocessing(obj):
-    # print(df)
-    obj = pandas.DataFrame(obj, index=[df.shape[0]])
-    print(df.shape)
+    obj = pandas.DataFrame(obj)
     print(obj)
-    new_df = pandas.concat([df, obj])
-    # print(new_df.tail(1))
-    scaler = MinMaxScaler()
-    new_df[['BMI', 'Age']] = scaler.fit_transform(new_df[['BMI', 'Age']])
-    print(new_df.tail(1))
-    return new_df.tail(1).drop('outcome', axis=1)
+    obj[['BMI', 'Age']] = scaler.transform(obj[['BMI', 'Age']])
+    print(obj)
+    return obj.drop('outcome', axis=1)
 
 def predict(data):
     print(data)
